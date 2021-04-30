4810d35866ef74b08b214e539854cf4145ad05f2
Valentin Garcia <vgarcia0587@icloud.com>
2021-04-29 22:05:05 -0400

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const PORT = process.env.PORT || 8080;
+const $PORT = process.env.PORT || 8080;
 const app = express();
 // let notesdb = require("./db/db.json");
 
